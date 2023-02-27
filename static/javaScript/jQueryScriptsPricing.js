5cc54bc70bd5a599ed2e8f51afeb21ca227ac3e4
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-02-27 21:40:26 +0300

    added jQuery function

--- static/javaScript/jQueryScriptsPricing.js
+++ static/javaScript/jQueryScriptsPricing.js
@@ -1,7 +1,19 @@
-$(function(){
+$(function () {
     $('.px-2').show(1500);
 
-    $('.card-img-top').hover(function(){
-        $(this).toggleClass();
-    }) //поиграться для исправления
+    $('.card-img-top').hover(function () {
+        $(this).css({
+            'position': 'absolute',
+            'z-index': '9999',
+            'transform': 'scale(1.5)',
+            'transition': 'transform 0.3s ease-in-out'
+        });
+    }, function () {
+        $(this).css({
+            'position': '',
+            'z-index': '',
+            'transform': '',
+            'transition': ''
+        });
+    });
 });
