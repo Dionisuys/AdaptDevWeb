94509c453ffe687c0f4631fc7a9caf62bce0fdfb
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-02-28 01:03:59 +0300

    added dark style button slime window click image

--- static/javaScript/check.js
+++ static/javaScript/check.js
@@ -1,7 +1,13 @@
-// Выбираем кнопку
-const btn = document.querySelector(".button-class");
-// Отслеживаем щелчок по кнопке
-btn.addEventListener("click", function () {
-  // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
-  document.body.classList.toggle("dark-theme");
+// // Выбираем кнопку
+// const btn = document.querySelector(".button-class");
+// // Отслеживаем щелчок по кнопке
+// btn.addEventListener("click", function () {
+//   // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
+//   document.body.classList.toggle("dark-theme");
+// });
+
+// добавляем обработчик события на кнопку переключения режима
+$('#dark-mode-toggle').on('click', function() {
+  // переключаем класс на body, чтобы изменить цветовую схему
+  $('body').toggleClass('dark-mode');
 });
--- static/javaScript/jQueryScriptsPricing.js
+++ static/javaScript/jQueryScriptsPricing.js
@@ -1,6 +1,13 @@
 $(function () {
-    $('.px-2').show(1500);
 
+    //Всплывабщая подсказка справа
+    $(".px-2").fadeIn(3000, function () {
+        $(".my-4").each(function (i) {
+            $(this).delay(500 * i).fadeIn(2000);
+        });
+    });
+
+    //Увеличение фотографии при наведении на неё
     $('.card-img-top').hover(function () {
         $(this).css({
             'position': 'absolute',
@@ -9,6 +16,56 @@ $(function () {
             'transition': 'transform 0.3s ease-in-out'
         });
     }, function () {
+        $(this).css({
+            'position': '',
+            'z-index': '',
+            'transform': '',
+            'transition': ''
+        });
+    });
+
+    $('.card-img-top').on('click', function () {
+        // Предотвращаем повторное нажатие на изображение, когда оно уже открыто
+        if ($('.overlay').length > 0) {
+            return;
+        }
+
+        var description = $(this).attr('data-description');
+        var overlay = $('<div class="overlay"></div>');
+        var popup = $('<div class="popup">' + description + '</div>');
+
+        // Добавляем наложение "затемнения"
+        overlay.appendTo('body');
+
+        // Добавляем всплывающее окно
+        popup.appendTo('body');
+
+        // Предотвращаем взаимодействие с элементами под наложением "затемнения"
+        $('body').css('overflow', 'hidden');
+
+        // Центрируем всплывающее окно по вертикали и горизонтали
+        var topOffset = ($(window).height() - popup.outerHeight()) / 2;
+        var leftOffset = ($(window).width() - popup.outerWidth()) / 2;
+        popup.css({
+            'top': topOffset + 'px',
+            'left': leftOffset + 'px'
+        });
+
+        // Закрытие всплывающего окна и наложения "затемнения"
+        $('.overlay, .popup').on('click', function () {
+            $('.overlay, .popup').remove();
+            $('body').css('overflow', 'auto');
+        });
+
+        // Предотвращаем закрытие всплывающего окна при клике на само окно
+        $('.popup').on('click', function (event) {
+            event.stopPropagation();
+        });
+    });
+
+    // Событие клика по изображению
+    $('.card-img-top').on('click', function () {
+        // Возвращаем изображение в исходное состояние
         $(this).css({
             'position': '',
             'z-index': '',
